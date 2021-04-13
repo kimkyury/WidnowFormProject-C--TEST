c1e38063082fa2ad854c10989fd94e5caaae558c
kimkyury <mandarining0918@gmail.com>
2021-04-13 18:11:37 +0900

    Creating [Dialog Box]

---
+++ labelPos/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labelPos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1. Press버튼을 누르면 Form2가 열리도록 작성하자
+            Form2 dlg = new Form2();
+            
+            // 2. Form1이 가진 정보를 dlg(Form2)에게 전송해주자
+            // 이러기 위해서 Form2의 코드에 함수 LabelX, LabelY, LabelText를 미리 작성했지.
+            dlg.LabelX = label1.Left;
+            dlg.LabelY = label1.Top;
+            dlg.LabelText = label1.Text;
+
+            // 3. dlg.showDialog()하면 대화상자가 열리며, 열린 창에서 DialogResult.OK가 들어오면 아래 정보가 넘어가도록 작성하자
+            //      -Form2 디자이너에서, Button1의 [속성-DialogResult]값을 [OK] 라고 설정하였음을 전제로 한다
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                label1.Left = dlg.LabelX;
+                label1.Top = dlg.LabelY;
+                label1.Text = dlg.LabelText;
+            }
+
+            // 끝나면 dlg 닫기.
+            dlg.Dispose();
+        }
+    }
+}
---
+++ labelPos/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labelPos
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // X좌표 입출력
+        public int LabelX{
+           //여기에는 Labelx의 값을 Form1에서 받아오는 코드를 쓴다
+            get{ return Convert.ToInt32(textBox1.Text);} // [전송] textBox1 값, Form에서는 위치를 숫자로 인식하니 Int로 변환
+            set{ textBox1.Text = value.ToString();} // [저장] 입력 값, text니까 ToString으로 받아주자.
+        }
+
+        // Y좌표 입출력
+        public int LabelY{
+            //여기에는 Labely의 값을 Form2에서 받아오는 코드를 쓴다
+            get{return Convert.ToInt32(textBox1.Text);} // [전송]
+            set{textBox2.Text = value.ToString();} // [저장]
+        }
+
+        // 문자열 입출력
+        public string LabelText
+        {
+            get { return textBox3.Text; } 
+            set { textBox3.Text = value; } //어차피 문자열일테니까 형변환 안 시켜줘도 되는 거지.
+        }
+    }
+}
