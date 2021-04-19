c81459d4fc50042434094092bc8a82ad7d25561c
kimkyury <mandarining0918@gmail.com>
2021-04-19 22:04:13 +0900

    Testing [BinaryStreamTest] with Serialization

--- BinaryStreamTest/BinaryStreamTest/Form1.Designer.cs
+++ BinaryStreamTest/BinaryStreamTest/Form1.Designer.cs
@@ -99,6 +99,7 @@ namespace BinaryStreamTest
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
--- BinaryStreamTest/BinaryStreamTest/Form1.cs
+++ BinaryStreamTest/BinaryStreamTest/Form1.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 // using 명령어 추가
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace BinaryStreamTest
 {
@@ -26,11 +28,20 @@ namespace BinaryStreamTest
         {
             //전역에서 생성한 Human객체에 대한 정보를 출력시키도록 하자
             FileStream fs = new FileStream(@"c:\temp\Kim.bin", FileMode.Create, FileAccess.Write);
-            BinaryWriter bw = new BinaryWriter(fs);
+
+
+            //BinaryWriter을 이용한 출력, 클래스의 객체 하나하나 출력 중
+            /* 
+            BinaryWriter bw = new BinaryWriter(fs);            
             bw.Write(Kim.Name);
             bw.Write(Kim.Age);
-            //kim.Wite(bw);
+            */
+            
 
+            //BinaryFormatter를 이용한 출력, 직렬화
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, Kim);
+            
             fs.Close();
         }
 
@@ -38,25 +49,43 @@ namespace BinaryStreamTest
         {
             label1.Text = "파일을 읽는 중";
             label1.Refresh();
-
             //사실 컴퓨터는 댑따 빠르지만 대기시간이 있는 척 Sleep걸어주자
             System.Threading.Thread.Sleep(1000);
             FileStream fs = new FileStream(@"c:\temp\Kim.bin", FileMode.Open, FileAccess.Read);
+
+
+            // BinaryReader을 이용한 저장
+            /*
             BinaryReader br = new BinaryReader(fs);
+            Kim = new Human(br.ReadString(), br.ReadInt32());
+             */
+
+
+            //BinaryFormatter를 이용한 저장, 직렬화의 반대 과정에서 캐스팅 요구됨. (Human)
+            BinaryFormatter bf = new BinaryFormatter();
+            Kim = (Human)bf.Deserialize(fs);
+
 
             // 불러온 정보를 Kim객체의 이름(String으로 변환), 나이(32비트로 변환)를 저장시키자.
-            Kim = new Human(br.ReadString(), br.ReadInt32());
             fs.Close();
+            label1.Text = Kim.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
             label1.Text = Kim.ToString();
         }
     }
 
+
+    // [Serializable] 표시를 꼭 해줘야함
+    [Serializable]
     public class Human
     {
         // public으로 하여 바로 뭐.. 출력시키도록 바꿔놓자. (원래는 private였지)
         public string Name;
         public int Age;
-        private float Temp;
+        [NonSerialized]private float Temp;
 
         public Human(string aName, int aAge)
         {
