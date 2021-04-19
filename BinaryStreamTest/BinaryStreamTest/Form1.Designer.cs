e7c82e760b64702f36d8a167dcd20b5be558e875
kimkyury <mandarining0918@gmail.com>
2021-04-19 21:50:51 +0900

    Testing [BinaryStreamTest

---
+++ BinaryStreamTest/BinaryStreamTest/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace BinaryStreamTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, -34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(32, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 136);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "[표시하겠습니다]";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(63, 208);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(373, 205);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 43);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "다른이름으로 저장하기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 125);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "불러오기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 452);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BinaryStreamTest/BinaryStreamTest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// using 명령어 추가
+using System.IO;
+
+namespace BinaryStreamTest
+{
+    public partial class Form1 : Form
+    {
+        //전역으로 Human 객체를 생성해놓자.  이름과 나이 정보를 가진다
+        public Human Kim = new Human("김규리", 20);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //전역에서 생성한 Human객체에 대한 정보를 출력시키도록 하자
+            FileStream fs = new FileStream(@"c:\temp\Kim.bin", FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(Kim.Name);
+            bw.Write(Kim.Age);
+            //kim.Wite(bw);
+
+            fs.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "파일을 읽는 중";
+            label1.Refresh();
+
+            //사실 컴퓨터는 댑따 빠르지만 대기시간이 있는 척 Sleep걸어주자
+            System.Threading.Thread.Sleep(1000);
+            FileStream fs = new FileStream(@"c:\temp\Kim.bin", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+
+            // 불러온 정보를 Kim객체의 이름(String으로 변환), 나이(32비트로 변환)를 저장시키자.
+            Kim = new Human(br.ReadString(), br.ReadInt32());
+            fs.Close();
+            label1.Text = Kim.ToString();
+        }
+    }
+
+    public class Human
+    {
+        // public으로 하여 바로 뭐.. 출력시키도록 바꿔놓자. (원래는 private였지)
+        public string Name;
+        public int Age;
+        private float Temp;
+
+        public Human(string aName, int aAge)
+        {
+            Name = aName;
+            Age = aAge;
+            Temp = 1.23f;
+        }
+
+        public override string ToString()
+        {
+            Temp += 1;
+            return "이름 : " + Name + ", 나이 : " + Age;
+        }
+    }
+}
