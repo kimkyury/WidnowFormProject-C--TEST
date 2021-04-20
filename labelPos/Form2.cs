daa0c4579275a5a4f99753f3557be58a2c994fca
kimkyury <mandarining0918@gmail.com>
2021-04-20 23:49:44 +0900

    Testing [Dialog Box]

--- labelPos/Form2.cs
+++ labelPos/Form2.cs
@@ -19,14 +19,14 @@ namespace labelPos
 
         // X좌표 입출력
         public int LabelX{
-           //여기에는 Labelx의 값을 Form1에서 받아오는 코드를 쓴다
+           //여기에는 Lable X의 값을 Form1에서 받아오는 코드를 쓴다
             get{ return Convert.ToInt32(textBox1.Text);} // [전송] textBox1 값, Form에서는 위치를 숫자로 인식하니 Int로 변환
             set{ textBox1.Text = value.ToString();} // [저장] 입력 값, text니까 ToString으로 받아주자.
         }
 
         // Y좌표 입출력
         public int LabelY{
-            //여기에는 Labely의 값을 Form2에서 받아오는 코드를 쓴다
+            //여기에는 Lable Y의 값을 Form2에서 받아오는 코드를 쓴다
             get{return Convert.ToInt32(textBox1.Text);} // [전송]
             set{textBox2.Text = value.ToString();} // [저장]
         }
